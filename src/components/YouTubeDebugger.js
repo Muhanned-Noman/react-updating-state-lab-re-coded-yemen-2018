a96360a31dbad54783babc4b438e670bae1a6141
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-12 17:11:24 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,47 @@
 // Code YouTubeDebugger Component Here
+
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      errors: [],
+      user: null,
+      settings: { bitrate: 8, video: { resolution: '1080p' } }
+    };
+}
+
+bitrateEvent = () => {
+    this.setState({
+      settings: Object.assign({}, this.state.settings, {
+        bitrate: 12
+    }),
+    });
+}
+
+resolutionEvent = () => {
+    this.setState({
+      settings: Object.assign({}, this.state.settings, {
+        video: Object.assign({}, this.state.settings.video, {
+resolution: '720p'
+    }),
+    });
+  }
+}
+
+  render() {
+    return (
+      <div>
+        <button class="bitrate" onClick={this.bitrateEvent}>bitrate</button>
+        <button class="resolution" onClick={this.resolutionEvent}>resolution</button>
+      </div>
+    );
+}
+
+  
+}
+
+
+export default YouTubeDebugger;
